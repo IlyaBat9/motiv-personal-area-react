814cab73f29f370c16c9456c3a89210baaed35b8
Anna Gromova <gromovaanka@gmail.com>
2019-10-20 03:19:21 +0500

    added account page

--- motiv-personal-area/src/App.js
+++ motiv-personal-area/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import {Route, Switch} from "react-router-dom";
 import Account from "./components/Account/Account";
-import Auth from "./components/Auth/Auth"
+import Auth from "./components/Auth/Auth";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
         <React.Fragment>
+            <Header />
             <Switch>
                 <Route path="/" exact component = {Account}/>
                 <Route path="/auth" component = {Auth}/>

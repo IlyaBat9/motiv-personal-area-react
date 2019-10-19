97db4bc3b9604d5495722811914f8deb967b0ada
toplenboren <toplenboren@yandex.ru>
2019-10-19 20:52:15 +0500

    Redirect if not logged in logic

--- motiv-personal-area/src/Store/Store.jsx
+++ motiv-personal-area/src/Store/Store.jsx
@@ -3,7 +3,6 @@ import AuthReducer from "./Reducers/AuthReducer";
 import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
-
 const rootReducer = combineReducers({
     AuthReducer,
 });
--- motiv-personal-area/src/components/Account/Account.jsx
+++ motiv-personal-area/src/components/Account/Account.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import {Redirect} from "react-router-dom";
+import {connect} from "react-redux";
 
-function Account() {
+function Account(props) {
+    if(!props.token){
+        return <Redirect to={"/auth"}/>
+    }
     return (
         <React.Fragment>
             <h1>Account</h1>
@@ -8,4 +13,10 @@ function Account() {
     );
 }
 
-export default Account;
+function MapStateToProps(store) {
+    return {
+        token: store.AuthReducer.token
+    }
+}
+
+export default connect(MapStateToProps, null)(Account);

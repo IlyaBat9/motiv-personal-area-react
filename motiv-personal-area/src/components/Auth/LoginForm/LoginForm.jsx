d467a945dcddbcd6ca5b77d6578e0c3d371c8b84
toplenboren <toplenboren@yandex.ru>
2019-10-19 19:46:25 +0500

    Add placeholders

---
+++ motiv-personal-area/src/components/Auth/LoginForm/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginForm() {
+    return (
+        <React.Fragment>
+            <h1>LoginForm</h1>
+        </React.Fragment>
+    );
+}
+
+export default LoginForm;
